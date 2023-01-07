a077f1b6583b400b97b7a73892d8e33ff2942f34
Ben Smith <benjaminsmith1981@googlemail.com>
2023-01-07 14:30:37 +0000

    added worked and web prco

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ dotenv.load_dotenv()
 if __name__ == '__main__':
     bot.run_discord_bot()
 
-    # app = Flask(__name__)
-    # app.run(host=os.environ.get("IP"),
-    #     port=int(os.environ.get("PORT")),
-    #     debug=os.environ.get("DEBUG"),
-    #     )
-# import routes
+    app = Flask(__name__)
+    app.run(host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=os.environ.get("DEBUG"),
+        )
+import routes
